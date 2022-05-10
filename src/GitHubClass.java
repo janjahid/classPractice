2d86073cc8b9070ed155d7d0bba8c19d48c30d37
janmohammd jahed <janmohammad.jahid@tekschool.us>
2022-05-09 23:45:06 -0500

    empty-repository

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -1,11 +1,13 @@
 
 public class GitHubClass {
 public static void main (String[]args) {
-int a = 10;
+int a = 15;
 int b = 20;
 int c = a+b;
 System.out.println(c);
-
+String name = "Asgraf";
+String lastName = "Khayen";
+System.out.println(name);
 
 
 }

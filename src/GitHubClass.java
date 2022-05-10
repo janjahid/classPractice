bde99d5ce1cb04de29b3947da7f7197eb82c0c5d
janmohammd jahed <janmohammad.jahid@tekschool.us>
2022-05-10 13:37:08 -0500

    https://github.com/janjahid/janjahid.git

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -5,9 +5,8 @@ int a = 15;
 int b = 20;
 int c = a+b;
 System.out.println(c);
-String name = "Asgraf";
-String lastName = "Khayen";
-System.out.println(name);
+
+
 
 
 }
---
+++ src/GitHubpackage/GitHudClassPr.java
@@ -0,0 +1,5 @@
+package GitHubpackage;
+
+public class GitHudClassPr {
+
+}

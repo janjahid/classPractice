29b041602afdce02821921ac4af3f73bf42b7cd0
janmohammd jahed <janmohammad.jahid@tekschool.us>
2022-05-12 19:43:42 -0500

    LabPractice

--- src/GitHubpackage/GitHudClassPr.java
+++ src/GitHubpackage/GitHudClassPr.java
@@ -1,5 +1,14 @@
 package GitHubpackage;
 
 public class GitHudClassPr {
+	private void syso() {
+	// TODO Auto-generated method stub
+String Name= "jan";
+String Lastname = "jahid";
+System.out.println("Name" + "LastName");
+
+
+}
+
 
 }
---
+++ src/GitHubpackage/LabPractice.java
@@ -0,0 +1,10 @@
+package GitHubpackage;
+
+public class LabPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
